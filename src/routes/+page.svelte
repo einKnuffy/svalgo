f772c77113c2d179f49f59af1061ff0ec9d4092c
einKnuffy <kjell.herzke@gmail.com>
2023-10-14 09:09:39 +0200

    coming soon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,23 @@
-<script>
-	let dataX = Array(20)
-		.fill(0)
-		.map((_, i) => i);
-	let dataY = Array(20)
-		.fill(0)
-		.map((_, i) => i);
+<script lang="ts">
+	enum Cell {
+		Empty = 0,
+		Start = 1,
+		Objective = 2,
+		Obstacle = 3
+	}
+
+	const gridSize = 25;
+	let grid = Array(gridSize)
+		.fill(Cell.Empty)
+		.map(() => Array(gridSize).fill(Cell.Empty));
 
 	let gameStarted = false;
 	let objectiveReached = false;
 	let startPos = [-1, -1];
 	let objectivePos = [-1, -1];
 
-	const distance2D = (x1, y1, x2, y2) => Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	const distance2D = (x1: number, y1: number, x2: number, y2: number) =>
+		Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 
 	const endGame = () => {
 		gameStarted = false;
@@ -30,9 +36,6 @@
 		[-1, -1] */
 	];
 
-	let allowedPaths = [];
-	let prohibitedPaths = [];
-
 	const moveCell = () => {
 		let shortestDistance = Infinity;
 		let offset = [0, 0];
@@ -45,8 +48,10 @@
 			}
 		});
 
+		grid[startPos[0]][startPos[1]] = Cell.Empty;
 		startPos[0] += offset[0];
 		startPos[1] += offset[1];
+		grid[startPos[0]][startPos[1]] = Cell.Start;
 	};
 
 	const startGame = async () => {
@@ -56,11 +61,11 @@
 		)
 			return;
 
+		console.log('Grid: ', grid);
+
 		console.log('Start: ', startPos);
 		console.log('Objective: ', objectivePos);
-
 		gameStarted = true;
-		console.log(distance2D(startPos[0], startPos[1], objectivePos[0], objectivePos[1]));
 
 		while (!objectiveReached) {
 			if (distance2D(startPos[0], startPos[1], objectivePos[0], objectivePos[1]) == 0) {
@@ -75,37 +80,29 @@
 		endGame();
 	};
 
-	let selectAllowedPath = false;
-	let selectProhibitedPath = false;
-
-	const setPositions = (x, y) => {
+	let selectObstacle = false;
+	const setPositions = (x: number, y: number) => {
 		if (gameStarted) return;
-
-		console.log(allowedPaths);
-		console.log(prohibitedPaths);
 
 		if (objectiveReached) {
 			startPos = [-1, -1];
 			objectivePos = [-1, -1];
 			objectiveReached = false;
+			grid = Array(gridSize)
+				.fill(Cell.Empty)
+				.map(() => Array(gridSize).fill(Cell.Empty));
 		}
 
-		if (
-			!allowedPaths.find((path) => path[0] == x && path[1] == y) &&
-			!prohibitedPaths.find((path) => path[0] == x && path[1] == y)
-		) {
-			if (selectAllowedPath) {
-				allowedPaths.push([x, y]);
-			} else if (selectProhibitedPath) {
-				prohibitedPaths.push([x, y]);
+		if (grid[x][y] == Cell.Empty) {
+			if (selectObstacle) {
+				grid[x][y] = Cell.Obstacle;
+			} else if (startPos[0] == -1 && startPos[1] == -1) {
+				startPos = [x, y];
+				grid[x][y] = Cell.Start;
+			} else if (objectivePos[0] == -1 && objectivePos[1] == -1) {
+				objectivePos = [x, y];
+				grid[x][y] = Cell.Objective;
 			}
-		}
-
-		if (selectAllowedPath || selectProhibitedPath) return;
-
-		if (startPos[0] == -1 && startPos[1] == -1) startPos = [x, y];
-		else if (objectivePos[0] == -1 && objectivePos[1] == -1) {
-			objectivePos = [x, y];
 		}
 	};
 </script>
@@ -120,18 +117,18 @@
 
 <div class="flex items-center justify-center">
 	<div class="flex bg-primary p-3 rounded-lg">
-		{#each dataX as coordX, idX}
+		{#each grid as row, coordX}
 			<div class="flex flex-col h-full">
-				{#each dataY as coordY, idY}
+				{#each row as cell, coordY}
 					<div
-						class="flex items-center justify-center md:w-8 md:h-8 sm:w-6 sm:h-6 {objectivePos[0] ==
-							coordX && objectivePos[1] == coordY
+						class="flex items-center justify-center md:w-8 md:h-8 sm:w-6 sm:h-6 {cell ==
+						Cell.Objective
 							? objectiveReached
 								? 'bg-green-500 hover:bg-green-500'
 								: 'bg-red-500 hover:bg-red-500'
-							: startPos[0] == coordX && startPos[1] == coordY
+							: cell == Cell.Start
 							? 'bg-white hover:bg-white'
-							: prohibitedPaths.find((path) => path[0] == coordX && path[1] == coordY)
+							: cell == Cell.Obstacle
 							? 'bg-blue-900'
 							: 'bg-primary-focus hover:bg-white hover:bg-opacity-20'} rounded-sm m-0.5"
 						id="{coordX}-{coordY}"
@@ -141,40 +138,15 @@
 				{/each}
 			</div>
 		{/each}
-		<!-- 	{#each dataX as coordX}
-			<div class="flex flex-col h-full">
-				{#each dataY as coordY}
-					<div
-						class={`flex items-center justify-center md:w-8 md:h-8 sm:w-6 sm:h-6 ${
-							objectivePos[0] == coordX && objectivePos[1] == coordY
-								? objectiveReached
-									? 'bg-green-500 hover:bg-green-500'
-									: 'bg-red-500 hover:bg-red-500'
-								: startPos[0] == coordX && startPos[1] == coordY
-								? 'bg-white hover:bg-white'
-								: 'bg-primary-focus hover:bg-white hover:bg-opacity-20 selection:bg-blue-300'
-						} rounded-sm m-0.5`}
-						class:selected={prohibitedPaths.find((path) => path[0] == coordX && path[1] == coordY)}
-						on:click={() => setPositions(coordX, coordY)}
-					/>
-				{/each}
-			</div>
-		{/each} -->
 	</div>
 	<div class="flex flex-col ml-3">
 		<button
-			class="btn btn-primary text-opacity-50"
-			on:click={() => (selectAllowedPath = !selectAllowedPath)}>Path</button
-		>
-		<button
 			class="btn btn-primary text-opacity-50 mt-2"
-			disabled={selectProhibitedPath}
-			on:click={() => (selectProhibitedPath = !selectProhibitedPath)}>Prohibited</button
+			on:click={() => (selectObstacle = !selectObstacle)}>Obstacle</button
 		>
 		<button
 			class="btn btn-success text-opacity-50 mt-12"
-			disabled={(startPos[0] == -1 && startPos[1] == -1) ||
-				(objectivePos[0] == -1 && objectivePos[1] == -1)}
+			disabled={gameStarted}
 			on:click={startGame}>Start</button
 		>
 	</div>
